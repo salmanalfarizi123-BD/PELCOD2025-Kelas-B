4fcb731b8df23e7d9b39e1f3687359c8dc38a076
salmanalfarizi123-BD <bonces612@gmail.com>
2025-09-13 11:37:26 +0700

    250441100139_salmanalfarizi_day1

---
+++ 250441100139_salmanalfarizi_day 1/pelcod.py
@@ -0,0 +1,29 @@
+# # nama = "salman al farizi" 
+# # nim = 250441100139
+# # semester = 1
+
+# # print("=====BIODATA MAHASISWA=====")
+# # print("hALLO, nama saya : "+ nama + "\n")
+# # print(f"NIM saya : {nim}\n")
+# # print("saya bekuliah di semester : {}".format(semester))
+
+# a = 10
+# b = 5
+# c = a + b # penjumlahan
+# d = a - b # pengurangan
+# e = a * b # perkalian
+# f = a / b # pembagian
+# g = a % b # sisa bagi
+# print("hasil penjumlahan:", c)
+# print("hasil pengurangan:", d)
+# print("hasil perkalian:", e)
+# print("hasil pembagian:", f)
+# print("hasil sisa bagi:", g)
+
+nama = input("masukkan nama anda: ")
+umur = input("masukkan umur anda: ")
+buah = input("masukkan buah favorit anda: ")
+
+print("nama saya adalah " + nama)
+print("umur saya adalah " + umur + "tahun")
+print("buah favorit saya adalah " + buah)
