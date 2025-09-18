99cdf59e9e26a75a1768a96efad1346b2851d41a
salmanalfarizi123-BD <bonces612@gmail.com>
2025-09-18 21:17:23 +0700

    Add files via upload

---
+++ tugas1pelcode.py
@@ -0,0 +1,13 @@
+
+
+tugas = float(input ("masukkan nilai tugas: "))
+uts = float (input ("masukkan nilai uts: "))
+uas = float (input("masukkann nilai uas: "))
+
+nilai_tugas = 0.30
+nilai_uts = 0.30
+nilai_uas = 0.40
+
+nilai_akhir = (tugas * nilai_tugas) + (uts * nilai_uts) + (uas * nilai_uas)
+
+print("nilai akhir andi adalah:", nilai_akhir)
